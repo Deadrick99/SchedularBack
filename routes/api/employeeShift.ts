1909327875d82540c7a8608e67dd83aa2a24a89c
William <33364879+Deadrick99@users.noreply.github.com>
2023-01-24 18:25:12 -0500

    pls

--- routes/api/employeeShift.ts
+++ routes/api/employeeShift.ts
@@ -1,6 +1,6 @@
 import express from "express"
 const router = express.Router();
-const employeeShiftController = require("../../controllers/employeeShiftController");
+const employeeShiftController = require("../../controllers/EmployeeShiftController");
 const ROLES_LIST = require("../../Config/roles_list");
 const verifyRoles = require("../../middleware/verifyRoles");
 
