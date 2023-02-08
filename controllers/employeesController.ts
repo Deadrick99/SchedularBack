35a25759e81e31858806157e183e90e23d1f169c
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-08 00:01:25 -0500

    changed get id to username

--- controllers/employeesController.ts
+++ controllers/employeesController.ts
@@ -21,12 +21,12 @@ const deleteEmployee =  async (req:Request, res:Response) =>{
     res.json(result)
 }
 const getEmployee = async (req:Request, res:Response) =>{
-    if(!req?.params?.id) {
-        return res.status(400).json({message:"Id is required"});
+    if(!req?.params?.userName) {
+        return res.status(400).json({message:"UserName is required"});
     }
-    const employee = await prisma.employee.findUnique({where:{id:req.params.id}});
+    const employee = await prisma.employee.findUnique({where:{id:req.params.userName}});
     if(!employee){
-        return res.status(400).json({message:`Could not find employee with ID: ${req.params.id}`})
+        return res.status(400).json({message:`Could not find employee with username: ${req.params.userName}`})
     }
     res.json(employee) 
 }
