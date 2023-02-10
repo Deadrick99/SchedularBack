ea54daadd35aa1c8167f60c4b2a31e39508b7544
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-10 13:36:25 -0500

    fixed error on get employeeAvail

--- controllers/employeeAvailController.ts
+++ controllers/employeeAvailController.ts
@@ -37,7 +37,7 @@ const createEmployeeAvail = async (req:Request,res:Response) =>{
 }
 const getEmployeeAvail = async (req:Request,res:Response) =>{
     if (!req?.params?.id) return res.status(400).json({message:'Id is required!'})
-    const avail = await prisma.employeeAvailability.findUnique({where:{id: req.params.id}})
+    const avail = await prisma.employeeAvailability.findUnique({where:{id: req.body.params.id}})
     if(!avail) return res.status(204).json({message:"Could not find requested Availibilty"})
     res.status(200).json(avail);
 }
