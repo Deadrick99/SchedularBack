8b7a098a04e0447f92a7d7f58d59c501b5ed55b6
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-11 13:06:18 -0500

    updated Schema

---
+++ prisma/migrations/20230211180535_store_and_u_ser/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Employee" ADD COLUMN     "availSet" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Store" ADD COLUMN     "daysSet" BOOLEAN NOT NULL DEFAULT false;
