6770a40a07722f92a566caa4aa30a68b70debfc2
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-18 00:19:23 -0500

    added deployed site to CORS

--- Config/allowedOrgins.ts
+++ Config/allowedOrgins.ts
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
+  "https://capable-kulfi-d10dab.netlify.app/login",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://127.0.0.1:5173",
--- middleware/credentials.ts
+++ middleware/credentials.ts
@@ -6,7 +6,7 @@ const credentials = (req:Request, res:Response, next:NextFunction) => {
   if (allowedOrigins.includes(origin)) {
 
     res.header("Access-Control-Allow-Credentials", "true");
-   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
+   res.header("Access-Control-Allow-Origin", "https://capable-kulfi-d10dab.netlify.app/login")
   }
   next();
 };
