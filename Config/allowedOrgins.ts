271b929cfed05d88b75fb7ba5ab9fc58f050b896
William <33364879+Deadrick99@users.noreply.github.com>
2023-03-22 00:44:27 -0400

    modified cors

--- Config/allowedOrgins.ts
+++ Config/allowedOrgins.ts
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://capable-kulfi-d10dab.netlify.app",
+  "https://fastschedule.netlify.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://127.0.0.1:5173",
